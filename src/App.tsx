12c1c7b02ecadb2dd41a56ca3cc950f2b1ff46f0
renzisheree <mapneverdie1@gmail.com>
2023-10-03 14:24:00 +0700

    sync again

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import Header from "./layout/Header";
 import Footer from "./layout/Footer";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavigationBar from "./components/NavigationBar";
-import BookingPage from "./page/BookingPage";
+// import BookingPage from "./page/BookingPage";
 import MainPage from "./page/MainPage";
 // import LoginPage from "./page/LoginPage";
 import RegisterPage from "./page/RegisterPage";
 import LoginPage from "./page/LoginPage";
+// import Calendar from "./components/Canendar";
+import BookingPage from "./page/BookingPage";
+import Test1 from "./components/Test1";
 
 function App() {
   return (
@@ -17,7 +20,7 @@ function App() {
       <Router>
         <Routes>
           <Route
-            path="/home"
+            path="/"
             element={
               <>
                 <MainPage></MainPage>
@@ -30,6 +33,10 @@ function App() {
             element={
               <>
                 <BookingPage></BookingPage>
+
+                {/* <Test1></Test1> */}
+
+                {/* <Calendar></Calendar> */}
               </>
             }
           ></Route>
